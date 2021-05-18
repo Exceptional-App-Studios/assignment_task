a5e8d4df6173d94d18342a8445f602748fd98139
vibhutipatel222 <vibhutipatel20.06@gmail.com>
2021-05-18 14:20:34 +0530

    completed task 5

--- lib/pages/task_5.dart
+++ lib/pages/task_5.dart
@@ -1,4 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:http/http.dart' as http;
+
+/*
+I have done this task using the package: charts_flutter, by getting json data using API, then converting it to a list of 
+Point objects which is a class that I made to represent a point on the graph.
+*/
 
 class Task5 extends StatefulWidget {
   static const routeName = 'task-five';
@@ -7,9 +17,159 @@ class Task5 extends StatefulWidget {
 }
 
 class _Task5State extends State<Task5> {
+  bool checkData = false;
+  bool load = false;
+  bool success = false;
+  var data;
+  var response;
+  getData() {
+    if (data.length == 0) {
+      return [];
+    }
+    return [
+      new charts.Series<Point, int>(
+        id: 'Points',
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        domainFn: (Point p, _) => p.x,
+        measureFn: (Point p, _) => p.y,
+        data: data,
+      )
+    ];
+  }
+
+  fetchData() async {
+    checkData = true;
+    setState(() {
+      load = true;
+    });
+    try {
+      response = await http.get(
+          Uri.https('exceptional-studios.herokuapp.com', 'api/graph-task'));
+      if (response.statusCode == 200) {
+        print('Success');
+
+        Iterable l = json.decode(response.body);
+        List<Point> points =
+            List<Point>.from(l.map((model) => Point.fromJson(model)));
+        points.sort((a, b) {
+          return a.x.compareTo(b.x);
+        });
+        data = points;
+        setState(() {
+          success=true;
+          load = false;
+        });
+      } else {
+        print(response.statusCode);
+        print('Failed');
+        setState(() {
+          success=false;
+          load = false;
+        });
+      }
+    } catch (e) {
+      print("Some error occured: " + e.toString());
+      setState(() {
+        success=false;
+        load = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    //Task 5: Showing Graph
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              AspectRatio(
+                aspectRatio: 1,
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: checkData
+                      ? load
+                          ? Center(child: CircularProgressIndicator())
+                          : success
+                              ? charts.LineChart(getData(),
+                                  defaultRenderer:
+                                      new charts.LineRendererConfig(
+                                          includePoints: true))
+                              : Container(
+                                  color: Colors.cyanAccent,
+                                  child: Center(child: Text('Failed!',style: TextStyle(fontSize: 20),)),
+                                )
+                      : Container(
+                          color: Colors.cyanAccent,
+                        ),
+                ),
+              ),
+              Spacer(),
+              Container(
+                margin: const EdgeInsets.only(bottom: 30),
+                width: 219,
+                height: 60,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50)),
+                    elevation: 5,
+                    primary: HexColor('#009154'),
+                  ),
+                  onPressed: fetchData,
+                  child: Text(
+                    'Data from API',
+                    style: TextStyle(fontSize: 15, color: Colors.white),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(bottom: 30),
+                width: 219,
+                height: 60,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50)),
+                    elevation: 5,
+                    primary: HexColor('#363636'),
+                  ),
+                  onPressed: () {
+                    data = [
+                      new Point(0, 0),
+                      new Point(1, 1),
+                      new Point(2, 1),
+                      new Point(3, 3),
+                    ];
+                    success=true;
+                    checkData = true;
+                    setState(() {});
+                  },
+                  child: Text(
+                    'Show local Data',
+                    style: TextStyle(fontSize: 15, color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Point {
+  Point(this.x, this.y);
+  final int x;
+  final int y;
+
+  static fromJson(json) {
+    Point p = new Point(json['x_axis'], json['y_axis']);
+    return p;
   }
 }
