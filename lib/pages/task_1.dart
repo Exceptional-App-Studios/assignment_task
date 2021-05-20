4dabe1d5aafe776735055e53c80029ffe6720e56
unknown <darji.harbansi@gmail.com>
2021-05-20 22:35:33 +0530

    task 1 2 and 7 completed

--- lib/pages/task_1.dart
+++ lib/pages/task_1.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:http/http.dart' as http;
 
 class TaskOne extends StatefulWidget {
   static const routeName = 'task-one';
@@ -7,55 +10,137 @@ class TaskOne extends StatefulWidget {
   _TaskOneState createState() => _TaskOneState();
 }
 
+class User {
+  final String name;
+  final String imageUrl;
+  final String email;
+  User({this.name, this.imageUrl, this.email});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+        name: json["name"], imageUrl: json["image_url"], email: json['email']);
+  }
+}
+
 class _TaskOneState extends State<TaskOne> {
+  bool isPressed = false;
+  Future getSingleUserData() async {
+    setState(() {
+      isPressed = true;
+    });
+    var responce = await http
+        .get(Uri.https('exceptional-studios.herokuapp.com', 'api/users/14'));
+    if (responce.statusCode == 200) {
+      final jsonData = jsonDecode(responce.body);
+
+      User user = User(
+        name: jsonData['name'],
+        imageUrl: jsonData['image_url'],
+        email: jsonData['email'],
+      );
+      print(user.name);
+      print(user.imageUrl);
+      print(user.email);
+      return user;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.spaceBetween, (Question: if this is used what should you remove from the code?)
-            children: [
-              SizedBox(height: 20),
-              CircleAvatar(
-                radius: 70,
-              ),
-              SizedBox(height: 15), //Image from API
-              Text(
-                'Name',
-                style: TextStyle(fontSize: 29, color: Colors.black),
-              ),
-              Spacer(),
-              Container(
-                width: 219,
-                height: 60,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50)),
-                    elevation: 10,
-                    primary: HexColor('#363636'),
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    'Load data from api',
-                    style: TextStyle(fontSize: 15, color: Colors.white),
-                  ),
+      body: isPressed
+          ? SafeArea(
+              child: Center(
+                child: Column(
+                  children: [
+                    FutureBuilder(
+                        future: getSingleUserData(),
+                        builder: (context, snapshot) {
+                          if (snapshot.data == null) {
+                            return Container(
+                              child: Center(
+                                child: CircularProgressIndicator(),
+                              ),
+                            );
+                          } else {
+                            return Container(
+                              child: Column(
+                                children: [
+                                  SizedBox(
+                                    height: 50,
+                                  ),
+                                  CircleAvatar(
+                                    backgroundImage: NetworkImage(
+                                        '${snapshot.data.imageUrl}'),
+                                    radius: 130,
+                                  ),
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  Text(
+                                    snapshot.data.name,
+                                    style: TextStyle(
+                                        fontSize: 30,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            );
+                          }
+                        }),
+                    SizedBox(
+                      height: 300,
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 30),
+            )
+          : Container(
+              padding: EdgeInsets.all(15),
+              child: Center(
                 child: Text(
-                  'Success Message',
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: HexColor('#009154'),
-                  ),
+                  'please Press below button for Loading Data from API.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
-              )
-            ],
-          ),
+              ),
+            ),
+      bottomNavigationBar: Container(
+        height: MediaQuery.of(context).size.height * 0.2,
+        child: Column(
+          children: [
+            Container(
+              height: 60,
+              width: 220,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                  elevation: 10,
+                  primary: HexColor('#363636'),
+                ),
+                onPressed: getSingleUserData,
+                child: Text(
+                  'Load data from API',
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+                padding: const EdgeInsets.only(bottom: 30),
+                child: isPressed
+                    ? Text(
+                        'Name successfully fetch from API',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: HexColor('#009154'),
+                        ),
+                      )
+                    : Text(''))
+          ],
         ),
       ),
     );
--- lib/pages/task_7.dart
+++ lib/pages/task_7.dart
@@ -1,15 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class TaskSeven extends StatefulWidget {
   static const routeName = 'task-seven';
   @override
   _TaskSevenState createState() => _TaskSevenState();
 }
+// Task 7: Send Dummy Notification
 
 class _TaskSevenState extends State<TaskSeven> {
+  FlutterLocalNotificationsPlugin flutterNOtification;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    var androidInitilize = new AndroidInitializationSettings('appstore');
+    var iosInitialize = new IOSInitializationSettings();
+    var initilizationSetting = new InitializationSettings(
+        android: androidInitilize, iOS: iosInitialize);
+    flutterNOtification = new FlutterLocalNotificationsPlugin();
+    flutterNOtification.initialize(initilizationSetting,
+        onSelectNotification: notificationSelected);
+  }
+
   @override
   Widget build(BuildContext context) {
-    //Task 7: Send Dummy Notification
-    return Scaffold();
+    Future _showNotification() async {
+      var androidNotification =
+          new AndroidNotificationDetails('channelID', "harbansi", 'Student');
+      var iosdetails = new IOSNotificationDetails();
+      var generalNotification = new NotificationDetails(
+          android: androidNotification, iOS: iosdetails);
+
+      await flutterNOtification.show(
+          0, 'task7', 'Successfully Completed Task!!', generalNotification);
+    }
+
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 250,
+                width: 250,
+                decoration: BoxDecoration(color: Colors.green),
+                child: TextButton(
+                  child: Text(
+                    'Send Notification',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  onPressed: _showNotification,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                alignment: Alignment.center,
+                height: 100,
+                width: 250,
+                child: Text(
+                  'Click this button to push the Notification!!',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
+
+  Future notificationSelected(String payload) async {}
 }
