d548a327ef056066e1565022b9a4900e0a5d4f2a
Palak Bera <palakbera2281@gmail.com>
2021-05-22 22:51:01 +0530

    Task 1 completed

--- lib/service/service.dart
+++ lib/service/service.dart
@@ -5,7 +5,15 @@ import 'package:assignment_app/models/user.dart';
 
 class ApiService {
 
+  Future<User> getUser() async {
+    final response = await http.get(
+        Uri.parse("https://exceptional-studios.herokuapp.com/api/users/1/"));
 
+    if (response.statusCode != 200) throw Exception();
+
+    return new User.fromJSon(json.decode(response.body));
+    
+  }
 
   Future<List<User>> getUserList() async {
     final res = await http
